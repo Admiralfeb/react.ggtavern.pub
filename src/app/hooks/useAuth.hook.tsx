4767c87dbe337eee31dbd9d68ce04a8ad2e66928
Zachary Bryant <Zachary@admiralfeb.net>
2020-08-27 21:09:32 -0500

    fix: rm firebase warning in console

--- src/app/hooks/useAuth.hook.tsx
+++ src/app/hooks/useAuth.hook.tsx
@@ -1,19 +1,17 @@
 import { useState, useEffect } from 'react';
-import firebase from 'firebase';
+import { auth } from 'firebase/app';
 
 export const useAuth = () => {
   const [user, setUser] = useState<firebase.User>();
   useEffect(() => {
     if (process.env.NODE_ENV !== 'test') {
-      const unregisterAuthObserver = firebase
-        .auth()
-        .onAuthStateChanged((user) => {
-          if (user) {
-            setUser(user);
-          } else {
-            firebase.auth().signInAnonymously();
-          }
-        });
+      const unregisterAuthObserver = auth().onAuthStateChanged((user) => {
+        if (user) {
+          setUser(user);
+        } else {
+          auth().signInAnonymously();
+        }
+      });
       return () => unregisterAuthObserver();
     }
   });
