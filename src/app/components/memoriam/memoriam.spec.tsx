6ec907eb143a9d8f1dd1bf8e8eeb3e704c4db0ef
Zachary Bryant <Zachary@admiralfeb.net>
2020-08-27 22:38:29 -0500

    test: rm tests

--- src/app/components/memoriam/memoriam.spec.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import { Memoriam } from './memoriam.component';
-
-test('renders', () => {
-  render(<Memoriam />);
-});
